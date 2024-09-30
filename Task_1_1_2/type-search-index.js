cabd670f9d5ddd83aac9abc6f4995ca05f94f64b
ryasuke1 <ryasuke1@users.noreply.github.com>
2024-09-30 13:51:40 +0000

    Deploying to gh-pages from @ ryasuke1/OOP@d06461624d6051cba0bc0547c6b41c8db172ded4 🚀

---
+++ Task_1_1_2/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes","url":"allclasses-index.html"},{"p":"ru.nsu.khubanov","l":"Card"},{"p":"ru.nsu.khubanov","l":"Game"},{"p":"ru.nsu.khubanov","l":"GetRandomCards"},{"p":"ru.nsu.khubanov","l":"Card.Rank"},{"p":"ru.nsu.khubanov","l":"Card.Suit"}]
